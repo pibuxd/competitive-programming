c53ffa0a42093c6447e7e040e75cf31e4f89ec29
pibuxd <bobrasio@gmail.com>
2021-05-21 12:31:10 +0200

    new contest

---
+++ CFORC/#721_D.2/C.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fastio ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+
+int main(){ fastio;
+  int t;
+  cin >> t;
+
+  while(t--){
+    
+  }
+}
