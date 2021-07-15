e914fbb26ebc82a96880c65f4c5d90a0e0660de0
pibuxd <pbublik@v-lo.krakow.pl>
2021-07-15 22:29:06 +0200

    I had a nice end to my day :)

---
+++ Algorithms/Miscellaneous/fibonacci.cpp
@@ -0,0 +1,35 @@
+// * Ways to calculate n'th Fibonacci numbers
+#include <bits/stdc++.h>
+using namespace std;
+
+// O(log(n))
+int fib_math(int n){
+  double phi = (1 + sqrt(5)) / 2;
+  return round(pow(phi, n) / sqrt(5)); 
+}
+
+// O(n)
+int fib_dp(int n){
+  vector<int> f(n+1);
+  f[0] = 0;
+  f[1] = 1;
+
+  for(int i = 2; i <= n; i++)
+    f[i] = f[i-1] + f[i-2];
+  return f[n];
+}
+
+// O(n)
+int fib_recursion(int n){
+  if(n <= 1)
+    return n;
+  return fib_recursion(n-1) + fib_recursion(n-2);
+}
+
+int main(){
+  int n;
+  cin >> n;
+  cout << fib_math(n);
+  cout << fib_dp(n);
+  cout << fib_recursion(n);
+}
