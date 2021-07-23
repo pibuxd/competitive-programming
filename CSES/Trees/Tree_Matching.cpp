fdc6a5671b47f3d7103a7e5e14631acf0070d5aa
pibuxd <pbublik@v-lo.krakow.pl>
2021-07-23 12:13:12 +0200

    .

--- CSES/Trees/Tree_Matching.cpp
+++ CSES/Trees/Tree_Matching.cpp
@@ -1,16 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define fastio ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
 
-struct Graph {
-  int n;
-  vector<vector<int>> G;
+int n, ans;
+vector<vector<int>> adj;
+vector<bool> done;
 
-  Graph(int _n){
-    n = _n;
-    G.resize(n+1);
-  }
+void dfs(int v, int p){
+  for(int x : adj[v])
+    if(x != p){
+      dfs(x, v);
+      if(!done[x] && !done[v])
+        done[x] = done[v] = 1, ans++;
+    }
 }
 
 int main(){
-
+  fastio;
+  cin >> n;
+  adj.resize(n+1);
+  done.resize(n+1);
+  for(int i = 1; i <= n-1; i++){
+    int a, b;
+    cin >> a >> b;
+    adj[a].push_back(b);
+    adj[b].push_back(a);
+  }
+  ans = 0;
+  dfs(1, 0);
+  cout << ans;
 }
