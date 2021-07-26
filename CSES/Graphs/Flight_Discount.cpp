d2d098aad1b33d34c24f67b1ba9c9b3c8728b6fc
pibuxd <pbublik@v-lo.krakow.pl>
2021-07-26 17:13:27 +0200

    dijkstra's, annoying

--- CSES/Graphs/Flight_Discount.cpp
+++ CSES/Graphs/Flight_Discount.cpp
@@ -3,39 +3,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define fastio ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define ll long long
 
 const long long INF = LONG_MAX;
-int n;
-vector<vector<pair<int, long long>>> G;
-vector<long long> dist;
+int n, m;
+vector<vector<array<ll, 2>>> G;
+vector<array<ll, 2>> dist;
 
-void dijkstra(int start){
-  dist[start] = 0;
-  priority_queue<pair<long long, pair<int, int>>> PQ; // dist, <node, max>
-  PQ.push({0, {start, 0}});
+void dijkstra(){
+  dist[1] = {0, 0};
+  priority_queue<array<ll, 2>> PQ;
+  PQ.push({0, 1});
 
   while(!PQ.empty()){
-    long long w = -PQ.top().first;
-    int v = PQ.top().second.first;
-    int maxw = PQ.top().second.second;
- 
+    ll w = -PQ.top()[0];
+    int v = PQ.top()[1];
     PQ.pop();
 
-    if(w != dist[v])
+    if(w != dist[v][0])
       continue;
 
-    for(auto x : G[v]){
-      long long add;
-      if(x.second > maxw){
-        add = x.second/2 + maxw - maxw/2;
-        maxw = x.second;
-      } else{
-        add = x.second;
+    for(array<ll, 2> x : G[v]){
+      if(dist[x[0]][0] > dist[v][0] + x[1] - dist[v][1]/2 + dist[v][0]){
+        dist[x[0]] = {dist[v][0] + x[1] - dist[v][1]/2 + dist[v][1], dist[x[0]][0]};
+        PQ.push({-dist[x[0]][0], x[0]});
       }
-      if(dist[x.first] > dist[v] + add){
-        dist[x.first] = dist[v] + add;
-        PQ.push({-dist[x.first], {x.first, maxw}});
+      else if(dist[x[0]][0] + dist[x[0]][1] - dist[x[0]][1]/2 > dist[v][0] + x[1]/2 - dist[v][1]/2 + dist[v][0]){
+        dist[x[0]] = {dist[v][0] + x[1]/2 - dist[v][1]/2 + dist[v][1], x[1]};
+        PQ.push({-dist[x[0]][0], x[0]});
       }
+      
+      
     }
   }
 }
@@ -43,19 +41,15 @@ void dijkstra(int start){
 int main(){ fastio;
   int m;
   cin >> n >> m;
-  int start = 1; // starting node
   G.resize(n+1);
-  dist.resize(n+1, INF);
+  dist.resize(n+1, {INF, 0});
 
   for(int i = 0; i < m; i++){
     int a, b, w;
     cin >> a >> b >> w;
     G[a].push_back({b, w});
-    //G[b].push_back({a, w});
   }
 
-  dijkstra(start);
-
-  long long ans = dist[n];
-  cout << ans;
+  dijkstra();
+  cout << dist[n][0];
 }
