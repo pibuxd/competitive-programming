1c11f3f71021baba99cd3415a3bec8dbe65a526f
pibuxd <pbublik@v-lo.krakow.pl>
2021-07-28 09:47:55 +0200

    two pointers example

---
+++ CFORC/Problem_set/Cellular_Network_1500.cpp
@@ -0,0 +1,29 @@
+// * Two pointers
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+
+int n, m;
+vector<int> a, b;
+
+int main(){
+  fastio;
+  cin >> n >> m;
+  a.resize(n);
+  b.resize(m);
+
+  for(int &x : a) cin >> x;
+  for(int &x : b) cin >> x;
+
+  int ans = 0, ai = 0, bi = 0;
+
+  while(ai < n){
+    while(bi < m-1 && abs(a[ai] - b[bi]) >= abs(a[ai] - b[bi+1])){
+      bi++;
+    }
+    ans = max(ans, abs(a[ai] - b[bi]));
+    ai++;
+  }
+
+  cout << ans;
+}
