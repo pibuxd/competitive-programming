c97a5849fc1ccd7e11a1d418d2f881185534dde4
pibuxd <pbublik@v-lo.krakow.pl>
2021-08-09 18:26:54 +0200

    topological sort and todo changes

--- CFORC/Problem_set/Fox_And_Names_1600.cpp
+++ CFORC/Problem_set/Fox_And_Names_1600.cpp
@@ -1,77 +1,75 @@
 #include <bits/stdc++.h>
 using namespace std;
- 
+#define fastio ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+
 int n;
-int e[27][27];
-string s[101];
-bool visited[27];
- 
-int getID(char c)
-{
-	if(c == ' ') return 0;
-	return c - 'a' + 1;
-}
- 
-int MAIN()
-{
-	memset(e, 0, sizeof(e));
-	for(int i = 1; i <= 26; i++)
-		e[0][i] = true;
-	cin >> n;
-	for(int i = 1; i <= n; i++)
-	{
-		cin >> s[i];
-		s[i] += " ";
-	}
-	for(int i = 1; i < n; i++)
-	{
-		int pos = 0;
-		while(s[i][pos] == s[i+1][pos]) pos ++;
-		e[getID(s[i][pos])][getID(s[i+1][pos])] = true;
-	}
-	for(int k = 0; k <= 26; k++)
-		for(int i = 0; i <= 26; i++)
-			for(int j = 0; j <= 26; j++)
-				e[i][j] |= e[i][k] & e[k][j];
-	bool haveCycle = false;
-	for(int i = 0; i <= 26; i++)
-		haveCycle |= e[i][i];
-	if(haveCycle)
-		cout << "Impossible" << endl;
-	else
-	{
-		memset(visited, 0, sizeof(visited));
-		for(int i = 0; i <= 26; i++)
-		{
-			int now = 0;
-			for(int j = 0; j <= 26; j++)
-			{
-				bool valid = (!visited[j]);
-				for(int k = 0; k <= 26; k++)
-					if(visited[k] == false && e[k][j])
-						valid = false;
-				if(valid)
-				{
-					now = j;
-					break;
-				}
-			}
-			if(i > 0)
-				cout << char('a' + now - 1);
-			visited[now] = true;
+vector<string> v;
+vector<vector<int>> G;
+vector<int> ind;
+
+bool compare_words(string &a, string &b){
+	int min_len = min(a.size(), b.size());
+
+	for(int i = 0; i < min_len; i++){
+		if(a[i] != b[i]){
+			G[a[i]-'a'].push_back(b[i]-'a');
+			ind[b[i]-'a']++;
+			break;
 		}
-		cout << endl;
+		else if(a[i] == b[i] && i == min_len-1 && b.size() < a.size())
+			return false;
 	}
-	return 0;
+	return true;
 }
- 
-int main()
-{
-	#ifdef LOCAL_TEST
-		freopen("in.txt", "r", stdin);
-		freopen("out.txt", "w", stdout);
-	#endif
-	ios :: sync_with_stdio(false);
-	cout << fixed << setprecision(16);
-	return MAIN();
+
+bool build(){
+	for(int i = 1; i < n; i++)
+		if(!compare_words(v[i-1], v[i]))
+			return false;
+	return true;
+}
+
+string ans(){
+	bool ok = build();
+	if(!ok)
+		return "Impossible";
+
+	string _ans = "";
+	queue<int> Q;
+
+	for(int i = 0; i < 26; i++)
+		if(!ind[i])
+			Q.push(i);
+
+	while(!Q.empty()){
+		int v = Q.front();
+		Q.pop();
+
+		_ans += (char)('a'+v);
+
+		for(int x : G[v]){
+			ind[x]--;
+			if(!ind[x]){
+				Q.push(x);
+			}
+		}
+	}
+
+	if(_ans.size() == 26)
+		return _ans;
+	else
+		return "Impossible";
+}
+
+int main(){
+  fastio;
+	cin >> n;
+	v.resize(n);
+	G.resize(26);
+	ind.resize(26);
+
+	for(string &str : v)
+		cin >> str;
+
+	cout << ans();
 }
