f0ec414971f0f3cde9ddd10aff7321da93aaa978
pibuxd <pbublik@v-lo.krakow.pl>
2021-08-27 20:41:59 +0200

    small to large merging

---
+++ CFORC/Problem_set/Lomsat_gelral.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+typedef long long ll;
+
+ll n;
+vector<ll> ans;
+vector<vector<ll>> G;
+vector<map<ll, ll>> M;
+
+void dfs(ll v, ll p){
+  for(ll x : G[v]){
+    if(x != p){
+      dfs(x, v);
+      ll a = v, b = x;
+
+      if(M[a].size() < M[b].size())
+        swap(M[a], M[b]);
+      for(auto i : M[b]){
+        if(!M[a][i.first]) M[a][i.first] = i.second;
+        else M[a][i.first] += i.second;
+      }
+    }
+  }
+
+  auto it = M[v].begin();
+  while(true){
+    ans[v] += it->first;
+    ll temp = it->second;
+    it++;
+    if(it->second < temp)
+      break;
+  }
+}
+
+int main(){
+  fastio;
+  cin >> n;
+
+  ans.resize(n+1);
+  G.resize(n+1);
+  M.resize(n+1);
+  
+  for(ll i = 1; i <= n; i++){
+    ll c;
+    cin >> c;
+    M[i][c] = 1;
+  }
+
+  for(ll i = 1; i <= n-1; i++){
+    ll a, b;
+    cin >> a >> b;
+    G[a].push_back(b);
+    G[b].push_back(a);
+  }
+
+  dfs(1, 0);
+
+  for(ll i = 1; i <= n; i++)
+    cout << ans[i] << " ";
+}
---
+++ CSES/Trees/Distinct_Colors.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+
+int n;
+vector<int> ans;
+vector<vector<int>> G;
+vector<bool> vis;
+vector<set<int>> S;
+
+void dfs(int v, int p){
+
+  for(int x : G[v])
+    if(x != p){
+      dfs(x, v);
+      int a = v, b = x;
+      if(S[a].size() < S[b].size())
+        swap(S[a], S[b]);
+      for(int i : S[b]) S[a].insert(i);
+    }
+  
+  ans[v] = S[v].size();
+}
+
+int main(){
+  fastio;
+  cin >> n;
+
+  vis.resize(n+1);
+  ans.resize(n+1);
+  G.resize(n+1);
+  S.resize(n+1);
+  
+  for(int i = 1; i <= n; i++){
+    int c;
+    cin >> c;
+    S[i].insert(c);
+  }
+
+  for(int i = 1; i <= n-1; i++){
+    int a, b;
+    cin >> a >> b;
+    G[a].push_back(b);
+    G[b].push_back(a);
+  }
+
+  dfs(1, 0);
+
+  for(int i = 1; i <= n; i++)
+    cout << ans[i] << " ";
+}
