079c3095c6a38fa13c9359e86d4307d9f3a6e6c8
pibuxd <bobrasio@gmail.com>
2021-10-03 21:52:09 +0200

    from staszic high school

--- POI/wifi.cpp
+++ POI/wifi.cpp
@@ -2,15 +2,19 @@
 using namespace std;
 #define fastio ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
 
-bool check(float x, int n, int m, const vector<int> &v){
-  int used = n-1;
-  float last = (float)v[m] - (float)x;
+int n, m;
+vector<float> v;
 
-  for(int i = m; i >= 1; i--){
-    if(fabs((float)last-(float)v[i]) > (float)x){
+bool check(float x){
+  int used = n-1;
+  float last = v[1] + x;
+
+  for(int i = 1; i <= m; i++){
+    if(used < 0) return false;
+    if(used >= m) return true;
+    if(fabs(last-v[i]) > x){
       used--;
-      // cout << used << ", ";
-      last = (float)v[i] - (float)x;
+      last = v[i] + x;
     }
   }
 
@@ -19,31 +23,41 @@ bool check(float x, int n, int m, const vector<int> &v){
   return false;
 }
 
+float _round(float x, float p){
+  float f = floor(x);
+  float dif = x - f;
+  while(dif - p >= 0){
+    dif -= p;
+  }
+
+  float ans = x - dif;
+  return ans;
+}
+
 int main(){
   fastio;
   int t;
   cin >> t;
-
+  
   while(t--){
-    int n, m;
     cin >> n >> m;
-    vector<int> v(m+1);
+    v.assign(m+1, 0.0);
 
     for(int i = 1; i <= m; i++)
       cin >> v[i];
+    sort(v.begin(), v.end());
 
-    float ans = 0.0, l = 0.0, r = 1000000.0, p = 0.0001;
-    while(fabs((float)l-(float)r) >= (float)p){
-      float mid = ((float)l+(float)r)/(float)2.0;
-      if(check(mid, n, m, v)){
-        ans = (float)mid;
-        r = (float)mid - (float)p;
+    float ans = 0.0, l = 0.0, r = 14185.6, p = 0.01;
+    while(fabs(l-r) >= p){
+      float mid = (l+r)/2.0;
+      if(check(mid)){
+        ans = mid;
+        r = mid;
       }
       else
-        l = (float)mid + (float)p;
-    }
-
-    cout << check((float)20.0, n, m, v);
-    cout << setiosflags(ios::fixed) << setprecision(1) << (float)ans << "\n";
+        l = mid;
+    }    
+  
+    cout << setiosflags(ios::fixed) << setprecision(1) << ans << "\n";
   }
 }
