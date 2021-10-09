aa482dcce33dc6d9783cc9b882bc0186bfed6257
pibuxd <bobrasio@gmail.com>
2021-10-09 17:49:13 +0200

    kwa 2022 second stage

---
+++ Satori/KWA-2022/2a.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+
+int n, m;
+vector<int> x, y;
+
+int solve(){
+  int pio = 0;
+  cin >> n >> m;
+  x.assign(m+1, 0);
+  y.assign(n+1, 0);
+
+  for(int i = 1; i <= n; i++){
+    for(int j = 1; j <= m; j++){
+      char c; cin >> c;
+      if(c == '.') continue;
+      pio++;
+      x[j]++;
+      y[i]++;
+    }
+  }
+
+  int mxX = 0, mxY = 0;
+
+  for(int i = 1; i <= n; i++){
+    mxX = max(mxX, x[i]);
+    mxY = max(mxY, y[i]);
+    
+  }
+
+  int ans;
+  ans = pio - mxX + pio - mxY;
+  return ans;
+}
+
+int main(){
+  fastio;
+  int t;
+  cin >> t;
+  while(t--){
+    cout << solve() << "\n";
+  }
+}
---
+++ Satori/KWA-2022/2c.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+
+int n;
+vector<int> v;
+
+bool check(int k){
+  int l = 1, r = 1;
+
+  int x = 0;
+  while(r <= k + 1){
+    if(v[r] < k + 1 && r != k + 1) x++;
+    r++;
+  }
+
+  while(r <= n){
+    if(x == 0 && v[l + k] >= 2*k + 1)
+      return true;
+    if(r == n) break;
+
+    if(v[l + k] < k + 1)
+      x++;
+    if(v[l] < k + 1)
+      x--;
+    if(v[r+1] < k + 1)
+      x++;
+    if(v[l+1 + k] < k + 1)
+      x--;
+    l++;
+    r++;
+  }
+
+  return false;
+}
+
+int solve(){
+  cin >> n;
+  v.assign(n+1, 0);
+
+  for(int i = 1; i <= n; i++)
+    cin >> v[i];
+  
+  int l, r, ans;
+  l = 1, r = 200000, ans = 0;
+
+  while(l <= r){
+    int m = (l+r)/2;
+    
+    if(check(m))
+      ans = m, l = m + 1;
+    else  
+      r = m - 1;
+  }
+
+  return ans;
+}
+
+int main(){
+  fastio;
+  int t;
+  cin >> t;
+  while(t--)
+    cout << solve() << "\n";
+}
