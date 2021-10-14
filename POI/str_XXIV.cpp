ab643cb8be79717ae5bebd12e64f099c27472ea3
pibuxd <bobrasio@gmail.com>
2021-10-14 15:28:39 +0200

    DP on tree continue

--- POI/str_XXIV.cpp
+++ POI/str_XXIV.cpp
@@ -1,94 +1,78 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define fastio ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+typedef long long ll;
 
-int n, parts;
-vector<vector<int>> G;
-vector<bool> blocked;
+ll n, ans;
+vector<vector<ll>> G;
+vector<ll> par, black;
+vector<bool> color;
 
-void initData(){
-  cin >> n;
+void dfs(ll v, ll p){
+  for(ll x : G[v]){
+    if(x == p) continue;
+
+    par[x] = v;
+    dfs(x, v);
+  }
+}
+
+void add(ll x){
+  color[x] = true;
+  black[par[x]]++;
+
+  ll neigh = G[x].size();
+  ll bl = black[x];
+  if(color[par[x]] && x != 1)
+    bl++;
   
-  parts = 0;
-  G = vector<vector<int>>(n+1);
-  blocked = vector<bool>(n+1);
+  ll to_add = neigh - bl;
+  
+  ans += to_add - 1;
+}
 
-  for(int i = 1, a, b; i < n; i++){
-    cin >> a >> b;
+void subtract(ll x){
+  color[x] = false;
+  black[par[x]]--;
+
+  ll neigh = G[x].size();
+  ll bl = black[x];
+  if(color[par[x]] && x != 1)
+    bl++;
+  
+  ll to_add = neigh - bl;
+
+  ans -= to_add - 1;
+}
+
+int main(){
+  fastio;
+  cin >> n;
+  G.resize(n+1);
+  black.resize(n+1);
+  par.resize(n+1);
+  color.resize(n+1);
+
+  for(ll i = 1; i <= n-1; i++){
+    ll a, b; cin >> a >> b;
     G[a].push_back(b);
     G[b].push_back(a);
   }
-}
+  dfs(1, 0);
 
-int numberOfParts(int node){
-  int v = abs(node);
-  int wolne = 0;
+  ans = 1;
 
-  if(node > 0){
-    blocked[v] = true;
-
-    if(G[v].size() == 1){
-      if(blocked[G[v][0]]){
-        parts--;
-      }
-
-      return parts;
-    }
-
-    for(int x : G[v]){
-      if(!blocked[x]){
-        wolne++;
-        parts++;
-      }
-    }
-
-    if(wolne == 0){
-      parts--;
-    }
-
-  } else{
-    blocked[v] = false;
-
-    if(G[v].size() == 1){
-      if(blocked[G[v][0]]){
-        parts++;
-      }
-
-      return parts;
-    }
-
-    for(int x : G[v]){
-      if(!blocked[x]){
-        wolne++;
-        parts--;
-      }
-    }
-
-    if(wolne == 0){
-      parts++;
-    }
-  }
-
-  return parts;
-}
-
-int main(){ ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-  initData();
-
-  int m, v;
+  ll m;
   cin >> m;
-
-  cin >> v;
-  parts = numberOfParts(v);
-  m--;
-
-  if(parts == 0){
-    parts = 1;
-  }
-
-  cout << parts << '\n';
-
   while(m--){
-    cin >> v;
-    cout << numberOfParts(v) << '\n';
+    ll z;
+    cin >> z;
+
+    if(z > 0)
+      add(z);
+    else
+      subtract(-z);
+
+    cout << ans << "\n";
   }
 }
