598f9ae8c58dea3f65f1f6c3d5a1c449d4f22ba4
pibuxd <bobrasio@gmail.com>
2021-11-04 15:03:29 +0100

    sort with custom comparator

---
+++ Algorithms/STL/sort_comparator.cpp
@@ -0,0 +1,29 @@
+// * sort with custom comparator
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+vector<string> a;
+
+bool comp(string &a, string &b){
+  int as = a.size();
+  int bs = b.size();
+
+  if(as == bs)
+    return a < b;
+  else
+    return as < bs;
+}
+
+int main(){
+  cin >> n;
+  a.assign(n, "");
+
+  for(int i = 0; i < n; i++)
+    cin >> a[i];
+
+  sort(a.begin(), a.end(), comp);
+  for(auto s : a)
+    cout << s << "\n";
+}
