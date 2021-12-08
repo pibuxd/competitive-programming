8cce24e30906a5312e1636a870dc3b3871460976
pibuxd <bobrasio@gmail.com>
2021-12-08 22:23:46 +0100

    first and second day of Potyczki Algorytmiczne 2021

---
+++ PA/2021/B/ora.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+
+int n, k, ile;
+long long ans;
+vector<int> a, occ;
+
+int main(){
+  fastio;
+  cin >> n >> k;
+  a.resize(n+1, 0);
+  occ.resize(n+1, 0);
+  ile = 0;
+  ans = 0;
+
+  int x = 0;
+  vector<int> to_erase, to_push;
+  for(int i = 1; i <= n; i++){
+    cin >> a[i], occ[a[i]]++;
+    if(occ[a[i]] == 1)
+      ile++;
+    if(i <= k && occ[a[i]] > 1){
+      to_erase.push_back(i);
+      x++;
+    }
+    else if(i > k && occ[a[i]] == 1 && x > 0){
+      to_push.push_back(i);
+      x--;
+    }
+  }
+
+  if(ile < k){
+    cout << "-1";
+    return 0;
+  }
+
+  reverse(to_erase.begin(), to_erase.end());
+  int _x = 0;
+  for(int v : to_erase){
+    ans += k - v - _x;
+    _x++;
+  }
+  for(int v : to_push){
+    ans += v - (k+1) + _x;
+    _x--;
+  }
+
+  cout << ans;
+}
---
+++ PA/2021/C/zak.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+// 1 bit - a
+// 2 bity - c
+// 3 bity - g
+// 4 bity - o
+
+int n;
+string s; 
+
+int main(){
+  fastio;
+  cin >> n >> s;
+  
+  int jed = 0;
+  for(char c : s)
+    if(c == '1')
+      jed++;
+  
+  jed -= 2 * n;
+  if(jed < n || jed > 4*n){
+    cout << "NIE";
+    return 0;
+  }
+
+  jed -= n;
+  
+  for(int i = 1; i <= n; i++){
+    if(jed - 3 >= 0)
+      cout << 'o', jed -= 3;
+    else if(jed - 2 >= 0)
+      cout << 'g', jed -= 2;
+    else if(jed - 1 >= 0)
+      cout << 'c', jed -= 1;
+    else
+      cout << 'a';
+  }
+}
