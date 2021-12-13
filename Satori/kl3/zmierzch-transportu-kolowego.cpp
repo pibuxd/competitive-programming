0c24ffc89f486f332d0d8971653d7b2cc8d31c2b
pibuxd <bobrasio@gmail.com>
2021-12-13 20:28:47 +0100

    dp on tree

--- Satori/kl3/zmierzch-transportu-kolowego.cpp
+++ Satori/kl3/zmierzch-transportu-kolowego.cpp
@@ -1,88 +1,78 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define fastio ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+typedef long long ll;
 
-int n;
-vector<vector<int>> G;
-vector<int> val, dp1, dp2; // 1 - bez, 2 - z
-vector<bool> use;
+struct V { ll t, f; };
 
-void dfs(int v, int p){
-  for(int x : G[v]){
+ll n;
+vector<vector<ll>> G, if_f, if_t;
+vector<ll> val, use;
+vector<V> dp;
+
+void dfs(ll v, ll p){
+  for(ll x : G[v]){
     if(x == p) continue;
-
     dfs(x, v);
-    // dp1[v] += min(dp1[x] + val[x], dp2[x]);
-    
-    // dp1[v] += dp2[x];
-    if(dp1[x] + val[x] <= dp2[x]){
-      dp1[v] += dp1[x] + val[x];
-    }
-    else{
-      dp1[v] += dp2[x];
-    }
-    
-    // dp2[v] += min(dp1[x], dp2[x]); 
-    if(dp1[x] <= dp2[x]){
-      dp2[v] += dp1[x];
-    }
-    else{
-      dp2[v] += dp2[x];
-    }
+
+    dp[v].t += min(dp[x].f, dp[x].t);
+    dp[v].f += dp[x].t;
   }
 }
 
-void dfs2(int v, int p){
-  if(dp1[v] <= dp2[v] && dp1[v] > 0){
-    use[v] = false;
+void dfs2(ll v, ll p, bool taken){
+  use[v] = taken;
+  for(ll x : G[v]){
+    if(x == p) continue;
+    
+    if(taken){
+      if(dp[x].f < dp[x].t)
+        dfs2(x, v, false);
+      else
+        dfs2(x, v, true);
 
-    for(int x : G[v]){
-      if(x == p) continue;
-      dfs2(x, v);
-    }
-  }
-  else{
-    for(int x : G[v]){
-      if(x == p) continue;
-      dfs2(x, v);
+    } else{
+      dfs2(x, v, true);
     }
   }
 }
 
 void solve(){
   cin >> n;
-  G.assign(n+1, vector<int>());
+  G.assign(n+1, vector<ll>());
+  if_f.assign(n+1, vector<ll>());
+  if_t.assign(n+1, vector<ll>());
   val.assign(n+1, 0);
-  dp1.assign(n+1, 0);
-  dp2.assign(n+1, 0);
-  use.assign(n+1, true);
+  use.assign(n+1, false);
+  dp.assign(n+1, V());
 
-  for(int i = 1; i <= n; i++){
+  for(ll i = 1; i <= n; i++){
     cin >> val[i];
-    dp2[i] = val[i];
+    dp[i].t = val[i];
   }
 
-  for(int i = 1; i <= n-1; i++){
-    int a, b;
+  for(ll i = 1; i <= n-1; i++){
+    ll a, b;
     cin >> a >> b;
     G[a].push_back(b);
     G[b].push_back(a);
   }
   
   dfs(1, -1);
-  dfs2(1, -1);
-
-  cout << min(dp1[1], dp2[1]) << "\n";
-  for(int i = 1; i <= n; i++)
+  
+  dfs2(1, -1, (dp[1].t < dp[1].f) ? true : false);
+  // for(ll i = 1; i <= n; i++) cout << "t , f : " << dp[i].t << " , " << dp[i].f << "\n";
+  // cout << "\n";
+  cout << min(dp[1].t, dp[1].f) << "\n";
+  for(ll i = 1; i <= n; i++)
     cout << use[i];
   cout << "\n";
 }
 
-int main(){
+signed main(){
   fastio;
-  int t;
+  ll t;
   cin >> t;
-
   while(t--)
     solve();
 }
