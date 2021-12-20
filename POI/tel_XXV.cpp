1e7012f0e72937a432c8423cb1e3316ef3c4da23
pibuxd <bobrasio@gmail.com>
2021-12-20 20:18:42 +0100

    segment tree

--- POI/tel_XXV.cpp
+++ POI/tel_XXV.cpp
@@ -1,83 +1,122 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define fastio ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
-#define mid ((l+r)/2)
-#define lc (2*v)
-#define rc (2*v+1)
-#define dlg(l, r) (r-l+1)
+#define int long long
+#define lv (2*v)
+#define rv (2*v+1)
+#define m ((l+r)/2)
 
 struct Node {
-  int sum = 0, ls = 0, la = 0;
+  int v = 0, lza = 0, lzq = 0;
 };
 
-int n, m;
+int n, q;
 vector<Node> t;
-vector<pair<int, int>> maszty;
+vector<array<int, 2>> maszty;
 
-void push_down(int v, int l, int r){
-  t[lc].ls = t[rc].ls = t[v].ls;
-  t[lc].la = t[rc].la = t[v].la;
-  t[lc].sum = ( (2*t[lc].ls + (dlg(l, mid) - 1) * t[lc].la) * dlg(l, mid) ) / 2;
-  t[rc].sum = ( (2*t[rc].ls + (dlg(mid+1, r) - 1) * t[rc].la) * dlg(mid+1, r) ) / 2;
+int an(int a, int q, int i){
+  return a + q*(i-1);
+}
+
+int sn(int len, int a, int q){
+  return ( ( 2*a + (len-1)*q ) * (len) ) / 2;
 }
 
 void push_up(int v){
-  t[v].sum = t[lc].sum + t[rc].sum;
+  t[v].v = t[lv].v + t[rv].v;
 }
 
-void update(int v, int l, int r, int a, int b, int S, int A, int op){
+void push_down(int v, int l, int r){
+  if(t[v].lza == 0 && t[v].lzq == 0)
+    return;
+
+  int A = t[v].lza;
+  int Q = t[v].lzq;
+  int _A = an(A, Q, m+1-l+1);
+
+  t[lv].lza += A;
+  t[lv].lzq += Q;
+  t[rv].lza += _A;
+  t[rv].lzq += Q;
+ 
+  t[lv].v += sn(m-l+1, A, Q);
+  t[rv].v += sn(r-m-1+1, _A, Q);
+
+  t[v].lza = t[v].lzq = 0;
+}
+
+void upd(int v, int l, int r, int a, int b, int A, int Q){
   if(l > b || r < a)
     return;
   if(a <= l && r <= b){
-    t[v].ls += S * op;
-    t[v].la += A * op;
-    t[v].sum = ( (2*t[v].ls + (dlg(l, r) - 1) * t[v].la) * dlg(l, r) ) / 2;
+    int __t = t[v].v;
+
+    int _A = an(A, Q, l-a+1);
+    t[v].lza += _A;
+    t[v].lzq += Q;
+    t[v].v += sn(r-l+1, _A, Q);
     return;
-  }
+  } 
 
   push_down(v, l, r);
-  update(lc, l, mid, a, b, S, A, op);
-  update(rc, mid+1, r, a, b, S+A*((mid+1)-l), A, op);
+  upd(lv, l, m, a, b, A, Q);
+  upd(rv, m+1, r, a, b, A, Q);
   push_up(v);
 }
 
-int query(int v, int l, int r, int a, int b){
+void upd(int x, int a, int q){
+  int zl = a / q;
+  int zr = a / q;
+
+  if(x - zl < 1)
+    zl = x-1;
+  if(x + zr > n)
+    zr = n-x;
+
+  int L = x - zl;
+  int R = x + zr;
+  int end_val_l = a - (q * zl);
+  upd(1, 1, n, L, x, end_val_l, q);
+  if(R > x)
+    upd(1, 1, n, x+1, R, a-q, -q);
+}
+
+int qry(int v, int l, int r, int a, int b){
   if(l > b || r < a)
     return 0;
   if(a <= l && r <= b)
-    return t[v].sum;
+    return t[v].v;
   
   push_down(v, l, r);
-  return query(lc, l, mid, a, b) + query(rc, mid+1, r, a, b);
+  return qry(lv, l, m, a, b) + qry(rv, m+1, r, a, b);
 }
 
-int main(){
-  fastio;
-  cin >> n >> m;
-  t.resize(4*(n+1));
-  maszty.resize(n+1);
+int qry(int a, int b){
+  return qry(1, 1, n, a, b) / (b-a+1);
+}
 
-  while(m--){
+signed main(){
+  fastio;
+  cin >> n >> q;
+  maszty.resize(n+1);
+  t.resize(4*(n+1));
+
+  while(q--){
     char op; cin >> op;
+    
     if(op == 'P'){
-      int x, s, a; cin >> x >> s >> a;
-      int l = max(1, x-(int)(ceil((float)s / (float)a))+1);
-      int r = min(n, x+(int)(ceil((float)s / (float)a))-1);
-      update(1, 1, n, l, x, a, a, 1);
-      update(1, 1, n, x+1, r, s - a, -a, 1);
-      maszty[x] = {s, a};
+      int x, a, q; cin >> x >> a >> q;
+      maszty[x] = {a, q}; 
+      upd(x, a, q);
 
     } else if(op == 'U'){
-      int x; cin >> x; int s = maszty[x].first, a = maszty[x].second;
-      int l = max(1, x-(int)(ceil((float)s / (float)a))+1);
-      int r = min(n, x+(int)(ceil((float)s / (float)a))-1);
-      update(1, 1, n, l, x, a, a, -1);
-      update(1, 1, n, x+1, r, s-a, -a, -1);
-      maszty[x] = {0, 0};
+      int x; cin >> x;
+      int a = maszty[x][0], q = maszty[x][1];
+      upd(x, -a, -q);
 
-    } else if(op == 'Z'){
-      int a, b; cin >> a >> b;
-      cout << query(1, 1, n, a, b) << "\n";
+    } else{
+      int x1, x2; cin >> x1 >> x2;
+      cout << qry(x1, x2) << "\n";
     }
   }
 }
