b417d0aa198a7a587edc6818d792969d8c0f2c6f
pibuxd <bobrasio@gmail.com>
2021-12-20 20:19:42 +0100

    x

--- Satori/kl3/kosmici.cpp
+++ Satori/kl3/kosmici.cpp
@@ -4,17 +4,25 @@ using namespace std;
 #define all(x) x.begin(), x.end()
 #define INF INT_MAX
 #define mxn 10001
+#define INF 10000
 
 struct Kosmita {
   int a = 0, b = 0, d = 0;
-
- bool operator < (const Kosmita &X){
-   return b < X.b;
- }
 };
 
-int n;
+int n, _n;
 vector<Kosmita> v;
+vector<vector<int>> dp;
+
+int mx(int x, int y){
+  if(x > y) swap(x, y);
+  int res = 0;
+  for(int i = 1; i <= n; i++)
+    if(v[i].a <= x && v[i].b <= y)
+      res = max(res, v[i].d);  
+
+  return res;
+}
 
 int main(){
   fastio;
@@ -24,44 +32,30 @@ int main(){
   while(t--){
     cin >> n;
     v.assign(n+1, Kosmita());
-    for(int i = 1; i <= n; i++)
+    _n = 15;
+    dp.assign(_n+2, vector<int>(_n+2, INF));
+
+    for(int i = 1; i <= n; i++){
       cin >> v[i].a >> v[i].b >> v[i].d;
-    
-    sort(all(v));
-    // for(int i = 1; i <= n; i++) cout << v[i].a << " " << v[i].b << " " << v[i].d << "\n";
-
-    vector<vector<array<int, 4>>> dp(n+1, vector<array<int, 4>>(mxn+1, {INF, INF, INF, INF})); // ans, b, d, old
-
-    for(int i = 1; i <= n; i++){
-      for(int j = 1; j <= mxn; j++){
-          if(v[i].d > j) continue;
-          if(dp[i-1][mxn][0] == INF){
-            dp[i][j] = {v[i].d, v[i].b, v[i].d, 0};
-            continue;
-          }
-
-          if(v[i].a <= dp[i-1][mxn][1]){ // mozna polaczyc z ciagiem poprzedniego
-            if(v[i].d > dp[i-1][mxn][2]){ // trzeba zwiekszyc moc
-              dp[i][j] = {dp[i-1][mxn][0] - dp[i-1][mxn][2] + v[i].d, dp[i-1][mxn][1], v[i].d, dp[i-1][mxn][0]};
-
-            } else{ // nie trzeba zwiekszyc mocy
-              dp[i][j] = dp[i-1][mxn];
-            }
-
-          } else{ // nie da sie dolepic
-            if(dp[i])
-            dp[i][j] = {dp[i-1][mxn][3] + v[i].d, v[i].b, v[i].d, dp[i-1][mxn][0]};
-          }
-            
-      }
     }
+    for(int i = 1; i <= _n; i++)
+      dp[i][i] = 0;
 
-    for(int i = 1; i <= n; i++){
-      // for(int j = 1; j <= 20; j++){
-        cout << dp[i][20][0] << " , " << dp[i][20][3] << " ";
-      // }
+    for(int k = 1; k <= _n; k++)
+      for(int i = 1; i <= _n; i++)
+        for(int j = 1; j <= _n; j++)
+          dp[i][j] = dp[i][j] != INF ? min(dp[i][j], dp[i][k] + dp[k][j] + mx(i, j)) : mx(i, j);
+        
+ 
+    for(int i = 1; i <= _n; i++) cout << "\t" << i;
+    cout << "\n";
+
+    for(int i = 1; i <= _n; i++){
+      cout << i << ":\t";
+      for(int j = 1; j <= _n; j++){
+        cout << dp[i][j] << "\t";
+      }
       cout << "\n";
     }
-    cout << dp[n][mxn][0] << "\n";
   }
 }
