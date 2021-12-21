1d7e58abed768639b91f8e8c218dbb3875ced721
pibuxd <bobrasio@gmail.com>
2021-12-21 20:34:29 +0100

    better generator

--- generator.cpp
+++ generator.cpp
@@ -4,20 +4,15 @@ using namespace std;
 #define fastio ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
 
 // @return random int in the range of (a, b)
-int random_int(int a, int b){
+int ran_int(int a, int b){
   return a + rand()%(b - a + 1);
 }
 
 // @return random string from the english alphabet
-string random_string(int n){
-  size_t s_len = 26;
-  char letters[s_len] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q',
-  'r','s','t','u','v','w','x',
-  'y','z'};
-
+string ran_str(int n, int len){
   string ran = "";
   for(int i = 0; i < n; i++) 
-    ran += letters[rand() % s_len];
+    ran += (int)'a' + (rand() % len);
 
   return ran;
 }
@@ -32,9 +27,8 @@ int main(){
   fastio;
   init_Seed();
 
-  int x, y;
-  x = random_int(0, 10);
-  y = random_int(0, 10);
-
-  cout << x << " " << y;
+  int x = ran_int(5, 10);
+  string s = ran_str(6, 3);
+  
+  cout << x << "\n" << s;
 }
