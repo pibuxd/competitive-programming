08216dc9f96c68275be39d9c5fd736bcb8666975
pibuxd <bobrasio@gmail.com>
2021-12-22 08:50:55 +0100

    POI 1 stage HASHING NO COLISIONS

--- POI/kor_XVII.cpp
+++ POI/kor_XVII.cpp
@@ -1,94 +1,57 @@
-#include "bits/stdc++.h"
+#include <bits/stdc++.h>
 using namespace std;
 #define fastio ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define int uint64_t
 
-struct StringHash {
-   const int p = 29, MOD = 1e9+1;
-   int n, maximum;
-   vector<int> hashes1, hashes2, powers;
+#define M ((uint64_t)(1ull<<61) - 1)
+#define P ((uint64_t)23)
 
-   StringHash(int _n){
-      n = _n;
-      hashes1.resize(n+1);
-      hashes2.resize(n+1);
-      powers.resize(n+1);
-      powers[0] = 1;
+int n;
+vector<int> a, p1, h1;
 
-      for(int i = 1; i <= n; i++){
-         powers[i] = powers[i-1] * p % MOD;
-      }
-   }
+int check(int k){
+  unordered_set<int> S1;
 
-   int generate(const vector<int> &sznury){
-      set<int> rodzaje;
-      
-      for(int i = 1; i <= n; i++){
-         int hsh = sznury[i] * p % MOD;
-         hashes1[i] = hsh;
-         hashes2[i] = hsh;
-         rodzaje.insert(hsh);
-      }
+  for(int i = 1; k*i <= n; i++){
+    int hs1 = (h1[k*i] - h1[k*(i-1)] + M) % M;
+    if(!S1.count(hs1))
+      S1.insert(hs1);
+  }
 
-      maximum = rodzaje.size();
-      return maximum;
-   }
+  return S1.size();
+}
 
-   int findDifferent(int x, const vector<int> &sznury){
-      set<int> rodzaje;
-      hashes1[1] = hashes2[1] = 0;
+signed main(){
+  fastio;
+  cin >> n;
+  a.resize(n+1);
+  p1.resize(n+1);
+  h1.resize(n+1);
 
-      for(int i = 1; i <= x; i++){
-         hashes1[1] += sznury[i] * powers[i+x-1] % MOD;
-         hashes2[1] += sznury[i] * powers[x+x-i] % MOD;
-         rodzaje.insert(hashes1[1]);
-         rodzaje.insert(hashes2[1]);
-      }
+  p1[0] = 1;
+  for(int i = 1; i <= n; i++){
+    cin >> a[i];
+    p1[i] = (i == 1) ? 1 : ((p1[i-1] * P) % M);
+  }
 
-      for(int i = x + 1; i <= n - x; i += x){
-         hashes1[i] -= sznury[i-1] * powers[(i-1)%x] % MOD;
-         hashes1[i] += sznury[i+x-1] * powers[(i+x-1)%x] % MOD;
-         hashes1[i] += sznury[i+x-2] * powers[(i+x-2)%x] % MOD;
-         rodzaje.insert(hashes1[i]);
+  for(int i = 1; i <= n; i++)
+    h1[i] = (h1[i-1] + (a[i] * p1[a[i]]) % M ) % M;
 
-         hashes2[i] -= sznury[i-1] * powers[(i-1)%x-x%x] % MOD;
-         hashes2[i] += sznury[i+x-1] * powers[(i+x-1)%x-x%x] % MOD;
-         hashes2[i] += sznury[i+x-2] * powers[(i+x-2)%x-x%x] % MOD;
-         rodzaje.insert(hashes2[i]);
-      }
+  int mx = 0;
+  vector<int> ans;
+  for(int i = 1; i <= n; i++){
+    int c = check(i);
 
-      return rodzaje.size();
-   }
-};
+    if(c == mx)
+      ans.push_back(i);
+    else if(c > mx){
+      ans.clear();
+      ans.push_back(i);
+      mx = c;
+    }
+  }
 
-int main(){
-   fastio;
-   int n;
-   cin >> n;
-
-   vector<int> sznury(n+1);
-   for(int i = 1; i <= n; i++){
-      cin >> sznury[i];
-   }
-
-   StringHash hashes(n);
-   int maximum = hashes.generate(sznury);
-   vector<int> ans = {1};
-
-   for(int i = 2; maximum <= n/i; i++){
-      int hsh = hashes.findDifferent(i, sznury);
-
-      if(maximum == hsh ){
-         ans.push_back(i);
-      } else if(maximum < hsh){
-         maximum = hsh;
-         ans.clear();
-         ans.push_back(i);
-      }
-   }
-
-   cout << maximum << ' ' << ans.size() << '\n';
-   
-   for(int i : ans){
-      cout << i << ' ';
-   }
+  cout << mx << " " << ans.size() << "\n";
+  for(auto res : ans)
+    cout << res << " ";
 }
