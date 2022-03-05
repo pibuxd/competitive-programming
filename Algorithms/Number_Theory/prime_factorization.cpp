1c25aa3ba19e71799e112eac365b575341b05814
pibuxd <bobrasio@gmail.com>
2022-03-05 15:11:04 +0100

    add prime factorization code

---
+++ Algorithms/Number_Theory/prime_factorization.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<pair<int, int>> factors(int n){
+  vector<pair<int, int>> ret;
+
+  for(int i = 2; i * i <= n; i++){
+    int cnt = 0;
+    while(n % i == 0){
+      cnt++;
+      n /= i;
+    }
+    if(cnt)
+      ret.push_back({i, cnt});   
+  }
+  
+  if(n > 1)
+    ret.push_back({n, 1});
+
+  return ret;
+}
+
+int main(){
+  int n;
+  cin >> n;
+
+  auto fac = factors(n);
+
+  for(auto i : fac)
+    cout << i.first << ", " << i.second << "\n";
+}
