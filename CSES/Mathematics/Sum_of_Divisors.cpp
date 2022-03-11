e2cb2793f0abf19084881c010670a9dae9be4e9d
pibuxd <bobrasio@gmail.com>
2022-03-11 17:48:37 +0100

    (hard) divisibility

---
+++ CSES/Mathematics/Sum_of_Divisors.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define int long long
+#define M (int)(1e9+7)
+
+int sum(int x){
+  return (((x)*(x+1))/2)%M;
+}
+
+void add(int &a, int b){
+  a = (a+b+M)%M;
+}
+
+signed main(){
+  fastio;
+  int n; cin >> n;
+
+  int ans = 0;
+
+  for(int i = 1; i*i <= n; i++){
+    int ile = (n/i)%M;
+    int _ile = (ile-i+1)%M;
+    
+    add(ans, (_ile*i)%M);
+    add(ans, (sum(ile)-sum(i-1)+M)%M);
+
+    if(ile >= i)
+      add(ans, -i);
+  }
+
+  cout << ans;
+}
