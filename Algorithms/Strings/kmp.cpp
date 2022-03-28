0c376ee1b1eeb1182a04aa9d3c749cc7f52033a4
pibuxd <bobrasio@gmail.com>
2022-03-28 10:43:34 +0200

    KMP string matching algorithm

---
+++ Algorithms/Strings/kmp.cpp
@@ -0,0 +1,32 @@
+// * KMP string matching algorithm
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> kmp(string &s){
+  int n = s.size();
+  vector<int> pi(n);
+
+  for(int i = 1; i < n; i++){
+    int j = pi[i-1];
+    while(j > 0 && s[i] != s[j])
+      j = pi[j-1];
+    if(s[i] == s[j])
+      j++;
+    pi[i] = j;
+  }
+
+  return pi;
+}
+
+int main(){
+  string _s, s, p;
+  cin >> _s >> p;
+  s = p + '#' + _s;
+
+  vector<int> pi = kmp(s);
+
+  int ans = 0;
+  for(int l : pi)
+    ans += (l == p.size());
+  cout << ans;
+}
---
+++ CSES/StringAlgorithms/String_Matching.cpp
@@ -0,0 +1,28 @@
+// * KMP string matching algorithm
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+
+int main(){
+  fastio;
+  string _s, s, p;
+  cin >> _s >> p;
+  s = p + '#' + _s;
+  int n = s.size(), m = p.size();
+
+  vector<int> pi(n);
+
+  for(int i = 1; i < n; i++){
+    int j = pi[i-1];
+    while(j > 0 && s[i] != s[j])
+      j = pi[j-1];
+    if(s[i] == s[j])
+      j++;
+    pi[i] = j;
+  }
+
+  int ans = 0;
+  for(int l : pi)
+    ans += (l == p.size());
+  cout << ans;
+}
