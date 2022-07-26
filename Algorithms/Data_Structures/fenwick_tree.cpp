82c8a5ece2edb352cdeff47fb275a0047e78b613
pibuxd <bobrasio@gmail.com>
2022-07-26 20:56:33 +0200

    better comments and impl

--- Algorithms/Data_Structures/fenwick_tree.cpp
+++ Algorithms/Data_Structures/fenwick_tree.cpp
@@ -1,57 +1,27 @@
 // author: Piotr "pibuxd" Bublik (https://github.com/pibuxd)
 // * Fenwick Tree or BIT (Binary Indexed Tree)
-// * RANGE add, POINT query
 #include <bits/stdc++.h>
 using namespace std;
-#define fastio ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
 
-int n, m;
-vector<int> tree;
+int n;
+vector<int> bit;
 
-// add x from k to n
 void add(int k, int x){
-  while(k <= n){
-    tree[k] += x;
-    k += k &- k;
-  }
+  for(; k <= n; k += k & (-k))
+    bit[k] += x;
 }
 
-void range_add(int l, int r, int val){
+// range add
+// when i use range add sum() becomes point query otherwise sum() is 1 -> k query
+void add(int l, int r, int val){
   add(l, val);
   add(r+1, -val);
 }
 
-// sum from 1 to k
-int query(int k){
+// sum from 1 to k or point query if use range add
+int sum(int k){
   int s = 0;
-  while(k > 0){
-    s += tree[k];
-    k -= k &- k; // get first bit on eg. 10 = 1010 -> 2^1
-  }
-
+  for(; k > 0; k -= k & (-k))
+    s += bit[k];
   return s;
-}
-
-int main(){
-  fastio;
-  cin >> n >> m;
-  tree.resize(n+2);
-  
-  for(int i = 1, p = 0, x; i <= n; i++, p = x){
-    cin >> x;
-    add(i, x - p);
-  }
-  while(m--){
-    int operation;
-    cin >> operation;
-    if(operation == 1){
-      int a, b, u;
-      cin >> a >> b >> u;
-      range_add(a, b, u);
-    } else{
-      int k;
-      cin >> k;
-      cout << query(k) << "\n";
-    }
-  }
 }
