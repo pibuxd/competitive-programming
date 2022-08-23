fc305178bae0bd579650ee1f0ee47ce33062ed9e
pibuxd <bobrasio@gmail.com>
2022-08-23 13:12:22 +0200

    add precomputed logarithm table

--- Algorithms/Data_Structures/sparse_table.cpp
+++ Algorithms/Data_Structures/sparse_table.cpp
@@ -1,5 +1,6 @@
 // author: Piotr "pibuxd" Bublik (https://github.com/pibuxd)
 // * Sparse Table (RMQ)
+// I use precomputed logarithm table to reduce logn in query()
 #include <bits/stdc++.h>
 using namespace std;
 #define fastio ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
@@ -7,16 +8,20 @@ using namespace std;
 const int LOGN = 19;
 int n, q;
 vector<vector<int>> sparse;
+vector<int> lg;
 
 void build(){
   for(int j = 1; j <= LOGN-1; j++)
     for(int i = 1; i + (1 << (j-1)) <= n; i++)
       sparse[i][j] = min(sparse[i][j-1], sparse[i+(1<<(j-1))][j-1]);
+  
+  for(int i = 2; i <= n; i++)
+    lg[i] = lg[i/2] + 1;
 }
 
 int query(int a, int b){
   int len = b - a + 1;
-  int j = floor(log2(len));
+  int j = lg[len];
   return min(sparse[a][j], sparse[b-(1<<j)+1][j]);
 }
 
@@ -24,6 +29,7 @@ int main(){
   fastio;
   cin >> n >> q;
   sparse.resize(n+1, vector<int>(LOGN));
+  lg.resize(n+1);
 
   for(int i = 1; i <= n; i++)
     cin >> sparse[i][0];
