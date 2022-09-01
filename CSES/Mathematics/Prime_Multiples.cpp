9e3d6d6b3adc7d144d33e3a37890a6891da1125a
pibuxd <bobrasio@gmail.com>
2022-09-01 21:53:25 +0200

    example of inclusion-exclusion

--- CSES/Mathematics/Prime_Multiples.cpp
+++ CSES/Mathematics/Prime_Multiples.cpp
@@ -1,4 +1,5 @@
 // inclusion-exclusion principle
+// https://cses.fi/problemset/task/2185
 #include <bits/stdc++.h>
 using namespace std;
 #define fastio ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
